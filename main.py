f13bb89021e9c2e17a3271219ace7fada99d5dca
mateospinetti <mateospinetti1@gmail.com>
2025-09-19 15:21:31 -0300

    added new data

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from dotenv import load_dotenv
 from langchain.agents import tool
 from langchain_core.prompts import PromptTemplate
-from langchain_core.tools import render_text_description
+from langchain_core.tools import render_text_description, Tool
 from langchain_openai import ChatOpenAI
 from langchain.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
+from langchain.schema import AgentAction, AgentFinish
+from typing import Union
 
 load_dotenv()
 
@@ -14,6 +16,13 @@ def get_text_length(text: str) -> int:
     print(f"get_text_length enter with {text=}")
     text = text.strip("'\n").strip('"')
     return len(text)
+
+def find_tool_by_name(tools: list[Tool], tool_name:str) -> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+        return None
+
 
 
 if __name__ == "__main__":
@@ -47,6 +56,13 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, stop=["\nObservation", "Observation:"] )
     agent = {"input": lambda x: x['input']} | prompt | llm | ReActSingleInputOutputParser()
 
-    res = agent.invoke({"input": "What is the text length of 'DOG' in characters?"})
-    print(res)
+    agent_step: Union[AgentAction, AgentFinish]= agent.invoke({"input": "What is the text length of 'DOG' in characters?"})
+    print(agent_step)
+    if isinstance(agent_step, AgentAction):
+        tool_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tool_name, tools)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(observation)
 
